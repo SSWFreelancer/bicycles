cedb401cab9e60656c7fab24aee5f6f73d280c5f
SSWFreelancer <97734977+SSWFreelancer@users.noreply.github.com>
2022-02-03 17:44:30 +0500

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -36,6 +36,13 @@ ibg();
 $(document).ready(function () {
    $('.header__burger').click(function (event) {
       $('.header__burger, .header__menu').toggleClass('active');
+      $('body').toggleClass('lock');
+   });
+});
+$(document).ready(function () {
+   $('.header__link').click(function (event) {
+      $('body').removeClass('lock');
+       $('.header__burger, .header__menu').removeClass('active');
    });
 });
 
